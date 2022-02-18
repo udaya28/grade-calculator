24b44046cd349d993c58318175f0c70e246db43c
udaya28 <54870625+udaya28@users.noreply.github.com>
2022-02-18 21:00:26 +0530

    test commit

--- api/hello.js
+++ api/hello.js
@@ -2,6 +2,6 @@
 export async function handler(event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: 'Hello world!' }),
+        body: JSON.stringify({ message: 'Hello world! test' }),
     }
 }
