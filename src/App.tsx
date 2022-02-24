c39bf5dcc2dc8e3c788c1b67683ad31c1d4dc40d
udaya28 <54870625+udaya28@users.noreply.github.com>
2022-02-24 20:19:52 +0530

    Revert "added basic folder structure"
    This reverts commit c925f930353080a83343e2d0faa94991cae79db5.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
+import logo from './logo.svg'
+import './App.css'
 import { URL } from './constants/config'
-import Header from './components/Header/Header'
-import './App.styles.css'
-import Main from './components/Main/Main'
 
 function App() {
     useEffect(() => {
@@ -12,11 +11,18 @@ function App() {
     }, [])
 
     return (
-        <div className="app min-h-screen">
-            <Header />
-            <div className="mx-4 md:mx-8 lg:mx-12 my-5">
-                <Main />
-            </div>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit
+                    <code>src/App.tsx</code>
+                    and save to reload.
+                </p>{' '}
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
         </div>
     )
 }
