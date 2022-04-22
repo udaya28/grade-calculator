4563f023fad095e7e12d0d3a21594222675831af
udaya28 <54870625+udaya28@users.noreply.github.com>
2022-04-22 22:13:51 +0530

    added controls to DetailsForm

--- src/components/Main/CalculateYourSGPA/DetailsForm/DetailsForm.tsx
+++ src/components/Main/CalculateYourSGPA/DetailsForm/DetailsForm.tsx
@@ -1,5 +1,6 @@
 import { Grid, MenuItem, TextField } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { URL } from '../../../../constants/config'
 
 const sxrop = {
     '& .MuiOutlinedInput-root': {
@@ -18,20 +19,86 @@ const sxrop = {
 }
 
 function DetailsForm() {
-    const colleges = ['None', 'VCET', 'AU']
-    const regulations = ['2016', '2018']
-    const departments = ['CSE','IT','ECE','MEC']
+    // const colleges = ['None', 'VCET', 'AU']
+    // const regulations = ['2016', '2018']
+    // const departments = ['CSE', 'IT', 'ECE', 'MEC']
+
+    const [colleges, setColleges] = useState(['None'])
+    const [regulations, setRegulations] = useState([''])
+    const [departments, setDepartments] = useState([''])
 
     const [selectedCollege, setSelectedCollege] = useState<string>('None')
-    // const [selectedRegulation, setSelectedRegulation] = useState('')
-    // const [selectedDepartment, setSelectedDepartments] = useState('')
+    const [selectedRegulation, setSelectedRegulation] = useState('')
+    const [selectedDepartment, setSelectedDepartment] = useState('')
 
     const handelChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         console.log(e.target.value)
         setSelectedCollege(e.target.value)
     }
 
+    const fetchCollege = async () => {
+        const response = await fetch(`${URL.FULL_URL}/data`)
+        const data = await response.json()
+        console.log(data.colleges)
+        setColleges(['None', ...data.colleges])
+    }
 
+    const fetchRegulation = async () => {
+        const response = await fetch(`${URL.FULL_URL}/data?college=${selectedCollege}`)
+        const data = await response.json()
+        console.log(data.regulations)
+        setRegulations(data.regulations)
+    }
+
+    const fetchDepartment = async () => {
+        const response = await fetch(`${URL.FULL_URL}/data?college=${selectedCollege}&regulation=${selectedRegulation}`)
+        const data = await response.json()
+        console.log(data.departments)
+        setDepartments(data.departments)
+    }
+
+    const fetchSemester = async () => {
+        const response = await fetch(
+            `${URL.FULL_URL}/data?college=${selectedCollege}&regulation=${selectedRegulation}&department=${selectedDepartment}`,
+        )
+        const data = await response.json()
+        console.log(data.semesters)
+    }
+
+    useEffect(() => {
+        fetchCollege()
+    }, [])
+
+    useEffect(() => {
+        if (selectedCollege !== 'None' && selectedCollege !== '') {
+            fetchRegulation()
+            setSelectedRegulation('')
+        }
+        if (selectedCollege === 'None' || selectedCollege === '') {
+            setSelectedRegulation('')
+        }
+    }, [selectedCollege])
+
+    useEffect(() => {
+        if (selectedCollege !== 'None' && selectedCollege !== '' && selectedRegulation !== '') {
+            fetchDepartment()
+            setSelectedDepartment('')
+        }
+        if (selectedRegulation === '') {
+            setSelectedDepartment('')
+            setSelectedRegulation('')
+        }
+    }, [selectedRegulation])
+
+    useEffect(() => {
+        if (selectedDepartment !== '') {
+            fetchSemester()
+        }
+    }, [selectedDepartment])
+
+    useEffect(() => {
+        console.log({ selectedCollege, selectedRegulation, selectedDepartment })
+    }, [selectedCollege, selectedRegulation, selectedDepartment])
 
     return (
         <>
@@ -64,6 +131,9 @@ function DetailsForm() {
                     fullWidth
                     InputProps={{ sx: { color: 'secondary.main' } }}
                     sx={sxrop}
+                    disabled={selectedCollege === 'None'}
+                    value={selectedRegulation}
+                    onChange={(e) => setSelectedRegulation(e.target.value)}
                 >
                     {regulations.map((regulation) => (
                         <MenuItem key={regulation} value={regulation}>
@@ -82,6 +152,9 @@ function DetailsForm() {
                     fullWidth
                     InputProps={{ sx: { color: 'secondary.main' } }}
                     sx={sxrop}
+                    disabled={selectedCollege === 'None' || selectedRegulation === ''}
+                    value={selectedDepartment}
+                    onChange={(e) => setSelectedDepartment(e.target.value)}
                 >
                     {departments.map((department) => (
                         <MenuItem key={department} value={department}>
