bcab0430c0c9f05d93900d6ab1f9de6c96ac37c0
udaya28 <54870625+udaya28@users.noreply.github.com>
2022-05-05 13:49:39 +0530

    added signin button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,28 @@
 import React from 'react'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import { Grid } from '@mui/material'
+import { Button, Grid, Typography } from '@mui/material'
 import { NavLink } from 'react-router-dom'
-
 import Logo from '../Logo/Logo'
-import NavMenu from './NavMenu/NavMenu'
 import ThemeToggle from './ThemeToggle/ThemeToggle'
-import NavItems from './NavItems/NavItems'
 
 interface Props {
     darkMode: boolean
     handleThemeChange: () => void
+    setLoginDialogOpen: (value: boolean) => void
 }
 
-function Header({ darkMode, handleThemeChange }: Props) {
-    const menuItems = [
-        { id: 1, name: 'SGPA', link: '#sgpa' },
-        { id: 2, name: 'CGPA', link: '#cgpa' },
-        { id: 3, name: 'ABOUT US', link: '#about-us' },
-        { id: 4, name: 'CONTACT US', link: '#contact-us' },
-    ]
+function Header({ darkMode, handleThemeChange, setLoginDialogOpen }: Props) {
+    // const menuItems = [
+    //     { id: 1, name: 'SGPA', link: '#sgpa' },
+    //     { id: 2, name: 'CGPA', link: '#cgpa' },
+    //     { id: 3, name: 'ABOUT US', link: '#about-us' },
+    //     { id: 4, name: 'CONTACT US', link: '#contact-us' },
+    // ]
+    const handleLoginClick = () => {
+        setLoginDialogOpen(true)
+    }
+
     return (
         <AppBar sx={{ backgroundColor: 'background.default' }} position="fixed">
             <Toolbar>
@@ -32,18 +34,28 @@ function Header({ darkMode, handleThemeChange }: Props) {
                     </Grid>
 
                     <Grid item xs={6}>
-                        <Grid container alignItems="center" justifyContent="flex-end">
-                            <Grid item xs={10} sx={{ display: { xs: 'none', sm: 'none', md: 'block' } }}>
+                        <Grid container alignItems="center" justifyContent="flex-end" columnSpacing={2}>
+                            {/* <Grid item xs={10} sx={{ display: { xs: 'none', sm: 'none', md: 'block' } }}>
                                 <NavItems menuItems={menuItems} />
+                            </Grid> */}
+
+                            {/* <Grid item>
+                                <Button variant="outlined">SIGN IN</Button>
+                            </Grid> */}
+
+                            <Grid item>
+                                <Button variant="contained" onClick={handleLoginClick}>
+                                    <Typography sx={{ color: '#FFFFFF', fontSize: '14px' }}>SIGN IN</Typography>
+                                </Button>
                             </Grid>
 
                             <Grid item>
                                 <ThemeToggle darkMode={darkMode} handleThemeChange={handleThemeChange} />
                             </Grid>
 
-                            <Grid item sx={{ display: { md: 'none' } }}>
+                            {/* <Grid item sx={{ display: { md: 'none' } }}>
                                 <NavMenu menuItems={menuItems} />
-                            </Grid>
+                            </Grid> */}
                         </Grid>
                     </Grid>
                 </Grid>
